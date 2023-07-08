75cba076a125fc1e86b191e132966eade23dcf27
chogigang <cbh2031@gmail.com>
2023-07-08 22:36:40 +0900

    플러터 test 06_28

--- tesx_2/lib/box.dart
+++ tesx_2/lib/box.dart
@@ -19,8 +19,8 @@ class MyApp extends StatelessWidget {
 
           home:Scaffold(
             appBar: AppBar(title: Text('앱입니다.'),),
-            body:  Align( //박스 위치 상세 정렬
-              alignment: Alignment.bottomLeft,
+            body:  Align( //박스 위치 상세 정렬 그냥 센터나 등등 사용해도 됨
+              alignment: Alignment.bottomLeft,//박스 위치 정렬 조정
               child: Container(
                 width: 150,height: 70, color: Colors.blue, //박스데코레이션을 사용하면 위 컬러를 사용할수가 없음 중복을 방지
                 // width: double.infinity//가로로 꽉차게 무한이 늘릴려고 할때 가로 길이를 부모박스를 넘지않는 선에서 무한으로 설정
@@ -42,3 +42,4 @@ class MyApp extends StatelessWidget {
     );
   }   
 }
+//박스위젯 정렬은 자손 을 어떻게 사용하냐에 따라 위치 선정이나 정렬방식이 많이 달라진다.
